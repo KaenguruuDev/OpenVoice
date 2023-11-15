daea3ce4e95059b14d51a1965b01bca945beff7a
KaenguruuDev <user.jakob@outlook.de>
2023-11-15 14:52:02 +0100

    Now its actually working

--- scripts/Launch.cs
+++ scripts/Launch.cs
@@ -6,13 +6,11 @@ public partial class Launch : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		DisplayServer.WindowSetSize(new Vector2I(265, 310));
-		DisplayServer.Center();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print(DisplayServer.WindowGetSize());
+		
 	}
 }
