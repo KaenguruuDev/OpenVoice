09f577be8205d59079536ae2408ce4691d2faf8a
KaenguruuDev <user.jakob@outlook.de>
2023-11-15 14:35:42 +0100

    Test

--- scripts/Launch.cs
+++ scripts/Launch.cs
@@ -1,16 +1,19 @@
 using Godot;
-using System;
+
+/*
+ Your typical launch window loading updates and similar.
+*/
 
 public partial class Launch : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		DisplayServer.WindowSetSize(new Vector2I(500, 500));
+		ProjectSettings.Borderless(true);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 }
