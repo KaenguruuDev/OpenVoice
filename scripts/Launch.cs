a5a61e2eb908a0b48f7cebe2463f726c5c39e70a
KaenguruuDev <user.jakob@outlook.de>
2023-11-15 14:42:12 +0100

    I think it's finally working now

--- scripts/Launch.cs
+++ scripts/Launch.cs
@@ -1,19 +1,16 @@
 using Godot;
-
-/*
- Your typical launch window loading updates and similar.
-*/
+using System;
 
 public partial class Launch : Node2D
 {
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		DisplayServer.WindowSetSize(new Vector2I(500, 500));
-		ProjectSettings.Borderless(true);
+		DisplayServer.WindowSetSize(new Vector2I(50, 50));
 	}
 
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print("OH NO OH NO");
 	}
 }
